7c7dd5e63423de521ea838e3641583b98b28348c
EDUARDOALMEIDARODRIGUES <eduaralmeidarodri@gmail.com>
2025-07-24 09:52:11 -0400

    nova branch

--- font-end/src/Ts/main.tsx
+++ font-end/src/Ts/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-/* import App from './App.tsx'
-import Header from "./Header.tsx"; */
+import App from './App.tsx'
+import Header from "./Header.tsx";
 import ContApi from './ContApi.tsx';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ContApi />  
-    {/* <header className="header ease-out z-10 
+    <header className="header ease-out z-10 
     mx-auto max-h-[100vh] bg-cover bg-no-repeat transition-all duration-[850ms] md:h-[100px]">
       <div className="absolute max-w-[full] h-full inset-0 bg-black opacity-50" />
       <Header />
@@ -19,5 +19,5 @@ createRoot(document.getElementById("root")!).render(
     <main>
       
     </main>
-   */}</StrictMode>
+   </StrictMode>
 );
